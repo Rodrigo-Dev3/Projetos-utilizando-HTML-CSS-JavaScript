845df19cabc50712cfd2e0059c5b0b56e7709ae0
Rodrigo Fronza <84408076+Rodrigo-Dev3@users.noreply.github.com>
2023-01-17 11:41:51 -0300

    Conteúdo recente

---
+++ Organizador de pedidos - Restaurante/Organizador.js
@@ -0,0 +1,44 @@
+let comida = document.querySelector('#comida')
+let bebida = document.querySelector('#bebida')
+let botao = document.querySelector('.botao')
+
+
+let resultadoPedidoC = document.querySelector('.resultadoPedidoComida')
+let resultadoPedidoB = document.querySelector('.resultadoPedidoBebida')
+
+
+botao.addEventListener('click', capturaComida)
+botao.addEventListener('click', capturaBebida)
+botao.addEventListener('click', adicionaPedido)
+
+
+function capturaComida(){
+    resultadoPedidoC.textContent = comida.value
+}
+
+function capturaBebida(){
+    resultadoPedidoB.textContent = bebida.value
+}
+
+
+let celula = document.querySelector('.tabela')
+
+function adicionaPedido(){
+    let linha = document.createElement('tr')
+    let coluna = document.createElement('td')
+    let coluna2 = document.createElement('td')
+    
+    let comi = resultadoPedidoC.textContent
+    let bebi = resultadoPedidoB.textContent
+
+    coluna.textContent = comi
+    coluna2.textContent = bebi
+
+    linha.appendChild(coluna)
+    linha.appendChild(coluna2)
+
+    let tabelaC = document.querySelector('.tabela')
+
+    tabelaC.appendChild(linha)
+
+}
